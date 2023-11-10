110b880f878012f3b30ab27ad67d86a2ed7890c8
김건희 <101076201+geonheek@users.noreply.github.com>
2023-11-10 22:59:40 +0900

    Create file_operations.py

---
+++ file_operations.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+
+def open_item(path):
+    try:
+        os.startfile(path)
+    except Exception as e:
+        print("Error:", e)
+
+def rename_item(current_path, new_name):
+    try:
+        os.rename(current_path, new_name)
+    except Exception as e:
+        print("Error:", e)
+
+def delete_item(path):
+    try:
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+        else:
+            os.unlink(path)
+        print(f"{path} 삭제")
+    except Exception as e:
+        print("Error:", e)
