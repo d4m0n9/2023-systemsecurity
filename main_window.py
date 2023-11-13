ecfe3fb9cc6c57b66600e0d9746a1b65c52b628a
yuu4172 <101076205+yuu4172@users.noreply.github.com>
2023-11-13 22:12:08 +0900

    탭 추가

--- main_window.py
+++ main_window.py
@@ -1,5 +1,5 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QTreeView, QVBoxLayout, QPushButton
+import sys, os, shutil
+from PyQt5.QtWidgets import QApplication, QWidget, QTreeView, QVBoxLayout, QPushButton, QInputDialog, QLineEdit, QMessageBox, QFileSystemModel, QTabWidget
 from PyQt5.QtCore import Qt
 from file_operations import open_item, rename_item, delete_item
 from sort_operations import sort_by_ext, sort_by_date
@@ -12,8 +12,11 @@ class Main(QWidget):
         self.path = "C:"
         self.index = None
 
-        self.tv = QTreeView(self)
+        self.tv1 = QTreeView(self)
+        self.tv2 = QTreeView(self)
+        self.tv3 = QTreeView(self)
         self.model = QFileSystemModel()
+        
         self.btnRen = QPushButton("이름 바꾸기")
         self.btnDel = QPushButton("파일 삭제")
         self.btnOpen = QPushButton("파일/폴더 열기")
@@ -21,7 +24,14 @@ class Main(QWidget):
         self.btnSortDate = QPushButton("수정 날짜순 정렬")
         self.btnFilterExt = QPushButton("확장자별 필터링")
         self.btnSearch = QPushButton("파일 검색")
-        self.layout = QVBoxLayout()
+        
+        self.tabWidget = QTabWidget()
+        self.tabWidget.setTabPosition(QTabWidget.West)
+        self.tab1 = QWidget()
+        self.tab2 = QWidget()
+        self.tab3 = QWidget()
+        self.tab4 = QWidget()
+        
         self.setUi()
         self.setSlot()
 
@@ -39,21 +49,49 @@ class Main(QWidget):
         self.setGeometry(300, 300, 700, 350)
         self.setWindowTitle("파일 탐색기")
         self.model.setRootPath(self.path)
-        self.tv.setModel(self.model)
-        self.tv.setColumnWidth(0, 250)
 
-        self.layout.addWidget(self.tv)
-        self.layout.addWidget(self.btnDel)
-        self.layout.addWidget(self.btnRen)
-        self.layout.addWidget(self.btnOpen)
-        self.layout.addWidget(self.btnSortExt)
-        self.layout.addWidget(self.btnSortDate)
-        self.layout.addWidget(self.btnFilterExt)
-        self.layout.addWidget(self.btnSearch)
-        self.setLayout(self.layout)
+        for tv in [self.tv1, self.tv2, self.tv3]:
+            tv.setModel(self.model)
+            tv.setColumnWidth(0, 250)
+
+        self.tab1_ui()
+        self.tab2_ui()
+        self.tab3_ui()
+
+        self.tabWidget.addTab(self.tab1, "파일 탐색기")
+        self.tabWidget.addTab(self.tab2, "파일 로그")
+        self.tabWidget.addTab(self.tab3, "악성 코드 스캔 및 진단")
+
+        self.layout = QVBoxLayout(self)  # 메인 레이아웃 생성
+        self.layout.addWidget(self.tabWidget)  # 메인 레이아웃에 탭 위젯 추가
+        self.setLayout(self.layout)  # 메인 레이아웃 설정
+
+
+    def tab1_ui(self):
+        layout = QVBoxLayout()
+        layout.addWidget(self.tv1)
+        layout.addWidget(self.btnDel)
+        layout.addWidget(self.btnRen)
+        self.tab1.setLayout(layout)
+
+    def tab2_ui(self):
+        layout = QVBoxLayout()
+        layout.addWidget(self.tv2)
+        layout.addWidget(self.btnOpen)
+        layout.addWidget(self.btnSortExt)
+        self.tab2.setLayout(layout)
+
+    def tab3_ui(self):
+        layout = QVBoxLayout()
+        layout.addWidget(self.tv3)
+        layout.addWidget(self.btnSortDate)
+        layout.addWidget(self.btnFilterExt)
+        self.tab3.setLayout(layout)
 
     def setSlot(self):
-        self.tv.clicked.connect(self.setIndex)
+        self.tv1.clicked.connect(self.setIndex)
+        self.tv2.clicked.connect(self.setIndex)
+        self.tv3.clicked.connect(self.setIndex)
         self.btnRen.clicked.connect(self.ren)
         self.btnDel.clicked.connect(self.rm)
         self.btnOpen.clicked.connect(self.open_item)
