39d907a0dc45e1d2c22aef5fbb313880c42a6633
김건희 <101076201+geonheek@users.noreply.github.com>
2023-11-14 21:03:17 +0900

    input_dialog.py

--- filter_operations.py
+++
@@ -1,7 +0,0 @@
-from PyQt5.QtWidgets import QInputDialog, QLineEdit
-
-def filter_by_ext(model):
-    text, res = QInputDialog.getText(None, "확장자별 필터링", "필터링할 확장자를 입력하세요. (예:.txt)", QLineEdit.Normal)
-    if res:
-        ext_filter_list = ["*" + text] 
-        model.setNameFilters(ext_filter_list)
