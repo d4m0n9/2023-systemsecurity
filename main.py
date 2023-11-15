bbe816b49af3be21cd1bc86f80b171e909928994
김건희 <101076201+geonheek@users.noreply.github.com>
2023-11-15 16:40:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from main_widget import Main
-from delete_file import delete_item
-from rename_file import rename_item
-from open_file import open_item
-from sort_by_ext import sort_by_ext
-from sort_by_date import sort_by_date
-from filter_by_ext import filter_by_ext
-from search_file import search_file
 
 def main():
     app = QApplication([])
