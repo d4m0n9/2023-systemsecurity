a22a9b286f2efee70bf6f498fae1f1011445dabe
김건희 <101076201+geonheek@users.noreply.github.com>
2023-11-15 16:30:54 +0900

    Update main.py
    7개의 파일로 기능을 분리하여 import하여 사용함

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from main_window import MainWindow
+from main_widget import Main
+from delete_file import delete_item
+from rename_file import rename_item
+from open_file import open_item
+from sort_by_ext import sort_by_ext
+from sort_by_date import sort_by_date
+from filter_by_ext import filter_by_ext
+from search_file import search_file
 
-if __name__ == "__main__":
+def main():
     app = QApplication([])
-    ex = MainWindow()
+    ex = Main()
     ex.show()
     sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
