67a955313e667ad23a30eb4542aebd8783c7e6d8
김건희 <101076201+geonheek@users.noreply.github.com>
2023-11-15 16:26:52 +0900

    rename_file.py
    이름 바꾸기 기능

--- input_dialog.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtWidgets import QInputDialog, QLineEdit
-
-class InputDialog(QInputDialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-    def get_text(self, title, label, text=QLineEdit.Normal):
-        return self.getText(self, title, label, text)
---
+++ rename_file.py
@@ -0,0 +1,20 @@
+import os
+from PyQt5.QtWidgets import QInputDialog, QLineEdit
+
+def rename_item(model, index):
+    os.chdir(model.filePath(model.parent(index)))
+    fname = model.fileName(index)
+    text, res = QInputDialog.getText(model, "이름 바꾸기", "바꿀 이름을 입력하세요.", QLineEdit.Normal, fname)
+
+    if res:
+        while True:
+            ok = True
+            for i in os.listdir(os.getcwd()):
+                if i == text:
+                    text, res = QInputDialog.getText(model, "중복 오류", "바꿀 이름을 입력하세요", QLineEdit.Normal, text)
+                    if not res:
+                        return
+                    ok = False
+            if ok:
+                break
+        os.rename(fname, text)
